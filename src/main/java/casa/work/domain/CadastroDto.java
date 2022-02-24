e32d9fb70bdea3c76ad25a5ba67a0d52321b75f2
joelson_luiz <joelsonluiz2010@gmail.com>
2022-02-24 10:51:21 -0300

    reparo de bugs e classe

--- src/main/java/casa/work/domain/CadastroDto.java
+++ src/main/java/casa/work/domain/CadastroDto.java
@@ -3,26 +3,19 @@ package casa.work.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@Entity
 public class CadastroDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
 	@NotBlank(message = "branco ou nulo não são aceitos")
 	@Size(min = 3, message = "mínimo de 3 caracteres")
 	private String nome;
 
 	private Integer idade;
-	
 
 	public CadastroDto() {
 
